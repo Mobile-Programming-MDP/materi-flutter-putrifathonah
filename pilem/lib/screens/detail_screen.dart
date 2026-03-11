a8f0519573b6bf3bfcc8eff150f9751474bbaef0
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2026-03-11 16:39:38 +0700

    add : favorite screen

--- pilem/lib/screens/detail_screen.dart
+++ pilem/lib/screens/detail_screen.dart
@@ -1,11 +1,69 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:pilem/models/movie.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final Movie movie;
+
   const DetailScreen({super.key, required this.movie});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  bool _isFavorite = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIsFavorite();
+  }
+
+  Future<void> _checkIsFavorite() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> favoriteMovies =
+        prefs.getStringList('favorite_movies') ?? [];
+
+    final isExist = favoriteMovies.any((movieString) {
+      final movieMap = jsonDecode(movieString);
+      return movieMap['id'] == widget.movie.id;
+    });
+
+    setState(() {
+      _isFavorite = isExist;
+    });
+  }
+
+  Future<void> _toggleFavorite() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> favoriteMovies =
+        prefs.getStringList('favorite_movies') ?? [];
+
+    final movieJson = jsonEncode(widget.movie.toJson());
+
+    if (_isFavorite) {
+      favoriteMovies.removeWhere((movieString) {
+        final movieMap = jsonDecode(movieString);
+        return movieMap['id'] == widget.movie.id;
+      });
+    } else {
+      favoriteMovies.add(movieJson);
+    }
+
+    await prefs.setStringList('favorite_movies', favoriteMovies);
+
+    setState(() {
+      _isFavorite = !_isFavorite;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final movie = widget.movie;
+
     return Scaffold(
       appBar: AppBar(title: Text(movie.title)),
       body: Padding(
@@ -14,11 +72,27 @@ class DetailScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.network(
-                'https://image.tmdb.org/t/p/w500${movie.backdropPath}',
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Stack(
+                children: [
+                  Image.network(
+                    'https://image.tmdb.org/t/p/w500${movie.backdropPath}',
+                    height: 300,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    right: 10,
+                    bottom: 10,
+                    child: IconButton(
+                      onPressed: _toggleFavorite,
+                      icon: Icon(
+                        _isFavorite ? Icons.favorite : Icons.favorite_border,
+                        color: Colors.red,
+                        size: 32,
+                      ),
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 20),
               const Text(
@@ -37,7 +111,7 @@ class DetailScreen extends StatelessWidget {
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(width: 10),
-                  Text(movie.releaseDate),
+                  Expanded(child: Text(movie.releaseDate)),
                 ],
               ),
               const SizedBox(height: 20),
--- pilem/lib/screens/favorite_screen.dart
+++ pilem/lib/screens/favorite_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detail_screen.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -8,8 +13,130 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
+  List<Movie> _favoriteMovies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFavoriteMovies();
+  }
+
+  Future<void> _loadFavoriteMovies() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> favoriteMoviesString =
+        prefs.getStringList('favorite_movies') ?? [];
+
+    setState(() {
+      _favoriteMovies = favoriteMoviesString
+          .map((movieString) => Movie.fromJson(jsonDecode(movieString)))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorite Movies'), centerTitle: true),
+      body: _favoriteMovies.isEmpty
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.favorite_border,
+                      size: 90,
+                      color: Colors.redAccent,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Belum ada yang fil favorite',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Tambahkan film ke favorit dari halaman detail film.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(12),
+              itemCount: _favoriteMovies.length,
+              itemBuilder: (context, index) {
+                final movie = _favoriteMovies[index];
+
+                return Card(
+                  margin: const EdgeInsets.only(bottom: 12),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(16),
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(movie: movie),
+                        ),
+                      );
+                      _loadFavoriteMovies();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.network(
+                              'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                              height: 80,
+                              width: 60,
+                              fit: BoxFit.cover,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Container(
+                                  height: 80,
+                                  width: 60,
+                                  color: Colors.grey.shade300,
+                                  child: const Icon(Icons.broken_image),
+                                );
+                              },
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+                          Expanded(
+                            child: Text(
+                              movie.title,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          const Icon(
+                            Icons.arrow_forward_ios,
+                            size: 18,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
   }
 }
