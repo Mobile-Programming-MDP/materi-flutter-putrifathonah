e234815add7d7311dceeb1ec43aeedd4e993b5e3
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2026-04-02 12:34:10 +0700

    sign in dan sign uo

---
+++ tapreport/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tapreport/firebase_options.dart';
+import 'package:tapreport/screens/home_screen.dart';
+import 'package:tapreport/screens/sign_in.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tap Report',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          } else {
+            return const SignInScreen();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ tapreport/lib/screens/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'sign_in.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => SignInScreen()),
+      (route) => false, //hapus semua route sebelumnya
+    );
+  }
+
+  String? _idToken = "";
+  String? _uid = "";
+  String? _email = "";
+  Future<void> getFirebasAuthUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      _uid = user.uid;
+      _email = user.email;
+      await user
+          .getIdTokenResult(true)
+          .then(
+            (v) => {
+              setState(() {
+                _idToken = v;
+              }),
+            },
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              signOut(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("You have been signed in with token id : ${_idToken!}"),
+            Text("CUrent User : ${_uid!}"),
+            Text("Current Email : ${_email}"),
+          ],
+        ),
+      ),
+    );
+  }
+}
